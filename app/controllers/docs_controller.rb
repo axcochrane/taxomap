08e199e322cb9dae782e290ebba44aae39ed850b
Sandy Cochrane <a.x.cochrane@gmail.com>
2020-07-08 10:18:32 +0100

    added Create New Doc functionality

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,4 +2,23 @@ class DocsController < ApplicationController
   def index
     @docs = Doc.all
   end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      flash.notice = 'Doc created successfully'
+      redirect_to docs_path
+    else
+      flash.alert = 'Error creating Doc'
+      redirect_to new_doc_poth
+    end
+  end
+
+  private
+
+  def doc_params
+    params.require(:doc).permit(
+      :title
+    )
+  end
 end
